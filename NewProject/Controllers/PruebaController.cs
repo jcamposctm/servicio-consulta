a1714e449fc5a06691d0d70cd558fc464c7250f3
CarlosEscMdz <58987129+CarlosEscMdz@users.noreply.github.com>
2020-02-24 11:01:58 -0500

    VistaVelvetJardin85
    Vista de la pantalla Velvet

---
+++ NewProject/Controllers/PruebaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewProject.Controllers
+{
+    [Route("Prueba")]
+    public class PruebaController : Controller
+    {
+        // GET: Prueba
+        [HttpGet]
+
+
+        public IActionResult Prueba()
+        {
+            return View();
+        }
+        
+
+        /*   public ContentResult Index()
+        {
+            return new ContentResult
+            {
+                ContentType = "text/html",
+                Content = "<div><p><h3> Control piscinas </h3><h3> Día </h3 ><h3> Fecha </h3></p></div>"
+            };
+        }  
+        */
+    }
+}
